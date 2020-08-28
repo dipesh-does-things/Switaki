aa5a2b2b96b5808043dd6615d9e23465978118d4
nathan-casabieille <nathan.casabieille@serviligne.com>
2020-08-28 10:13:57 +0200

    function which allows to retrieve the contents of a register

---
+++ src/cpu/registers/get_register_value.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020, Nathan Casabieille
+ *
+ * This file is part of Switaki
+ *
+ * Switaki is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Switaki is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * with Switaki.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "switaki.h"
+
+uint8_t get_reg8_value(cpu_t *cpu, uint8_t reg16, uint8_t reg8)
+{
+    if (reg8 >= REG_AH) {
+        return (cpu->registers[reg16] >> 8);
+    }
+    return (cpu->registers[reg16] & UINT16_MAX);
+}
+
+inline __attribute__((always_inline)) uint16_t get_reg16_value(cpu_t *cpu, uint8_t reg16)
+{
+    return (cpu->registers[reg16]);
+}
