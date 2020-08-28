07f0aeed5bdad27afd305787d0010d7d6b44d73f
nathan-casabieille <nathan.casabieille@serviligne.com>
2020-08-28 10:20:56 +0200

    function which retrieves a bit in the binary file (instruction) at a specific address

---
+++ src/instructions/fetch_instruction.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020, Nathan Casabieille
+ *
+ * This file is part of Switaki
+ *
+ * Switaki is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Switaki is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * with Switaki.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "switaki.h"
+
+inline uint16_t __attribute__((always_inline)) __attribute__((hot)) fetch_instruction(uint16_t address)
+{
+    return (image[address]);
+}
