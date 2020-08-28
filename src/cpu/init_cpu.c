2675b343d76e5b0c1c075db94f21b6e2e5b4919a
nathan-casabieille <nathan.casabieille@serviligne.com>
2020-08-28 10:06:48 +0200

    functions to create and destroy a cpu_t object

---
+++ src/cpu/init_cpu.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2020, Nathan Casabieille
+ *
+ * This file is part of Switaki
+ *
+ * Switaki is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Switaki is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * with Switaki.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "switaki.h"
+
+cpu_t *init_cpu(void)
+{
+    cpu_t *cpu;
+
+    cpu = malloc(sizeof(cpu));
+    if (!cpu) {
+        return (NULL);
+    }
+    cpu->memory = init_memory();
+    cpu->registers = init_registers();
+    if (!cpu->memory || !cpu->registers) {
+        destroy_cpu(cpu);
+        return (NULL);
+    }
+    return (cpu);
+}
